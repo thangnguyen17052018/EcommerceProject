44a20673c04bcca67bc297eec6c20848dfa2b241
nminhthang <rlrisnguyen@gmail.com>
2022-02-08 18:04:12 +0700

    create db, config data source, code role entity, role repository, admin home page.

---
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/nminhthang/admin/user/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.nminhthang.admin.user;
+
+import com.nminhthang.common.entity.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Integer> {
+
+}
