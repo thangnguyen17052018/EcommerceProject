70ed77df5bf0bbec20d84574cac1de50c2f51747
nminhthang <rlrisnguyen@gmail.com>
2022-02-10 18:09:34 +0700

    continue with user module: create new user function, encode user password, ...

--- EcommerceWebParent/EcommerceBackEnd/src/main/java/com/nminhthang/admin/user/UserController.java
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/nminhthang/admin/user/UserController.java
@@ -1,10 +1,13 @@
 package com.nminhthang.admin.user;
 
+import com.nminhthang.common.entity.Role;
 import com.nminhthang.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -20,5 +23,25 @@ public class UserController {
         model.addAttribute("listUsers", listUsers);
         return "users";
     }
+
+    @GetMapping("/users/new")
+    public String newUser(Model model){
+        List<Role> listRoles = userService.listRoles();
+        User user = new User();
+        user.setEnabled(true);
+
+        model.addAttribute("user", user);
+        model.addAttribute("listRoles", listRoles);
+
+        return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes){
+        userService.save(user);
+        redirectAttributes.addFlashAttribute("message", "The user was created successfully");
+        return "redirect:/users";
+    }
+
 
 }
--- EcommerceWebParent/EcommerceBackEnd/src/main/java/com/nminhthang/admin/user/UserService.java
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/nminhthang/admin/user/UserService.java
@@ -1,7 +1,9 @@
 package com.nminhthang.admin.user;
 
+import com.nminhthang.common.entity.Role;
 import com.nminhthang.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,8 +14,28 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     public List<User> listAll(){
         return (List<User>) userRepository.findAll();
     }
 
+    public List<Role> listRoles(){
+        return (List<Role>) roleRepository.findAll();
+    }
+
+
+    public void save(User user) {
+        encodePassword(user);
+        userRepository.save(user);
+    }
+
+    private void encodePassword(User user){
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+    }
 }
