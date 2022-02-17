3eaee6394026bff0cbf96a7b858667f3d1bd70d8
nminhthang <rlrisnguyen@gmail.com>
2022-02-17 18:31:23 +0700

    upload photo function

--- EcommerceWebParent/EcommerceBackEnd/src/main/java/com/nminhthang/admin/user/UserController.java
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/nminhthang/admin/user/UserController.java
@@ -1,16 +1,19 @@
 package com.nminhthang.admin.user;
 
+import com.nminhthang.admin.FileUploadUtil;
 import com.nminhthang.common.entity.Role;
 import com.nminhthang.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -63,9 +66,16 @@ public class UserController {
     }
 
     @PostMapping("/users/save")
-    public String saveUser(User user, RedirectAttributes redirectAttributes){
-        userService.save(user);
-        redirectAttributes.addFlashAttribute("message", "The user was created successfully");
+    public String saveUser(User user, RedirectAttributes redirectAttributes, @RequestParam("image") MultipartFile multipartFile) throws IOException {
+        System.out.println(user);
+        System.out.println(multipartFile.getOriginalFilename());
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+
+        String uploadDir = "user-photos";
+
+        FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+//        userService.save(user);
+//        redirectAttributes.addFlashAttribute("message", "The user was created successfully");
         return "redirect:/users";
     }
 
