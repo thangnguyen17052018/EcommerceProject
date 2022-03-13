3f3e36fb00031131dff3c96c499c678f26a5342a
NguyenMinhThang <rlrisnguyen@gmail.com>
2022-03-13 11:01:02 +0700

    category module + mockito test + unit test

--- EcommerceWebParent/EcommerceBackEnd/src/test/java/com/nminhthang/admin/category/CategoryServiceTests.java
+++ EcommerceWebParent/EcommerceBackEnd/src/test/java/com/nminhthang/admin/category/CategoryServiceTests.java
@@ -34,10 +34,105 @@ public class CategoryServiceTests {
                                     .build();
 
         Mockito.when(categoryRepository.findCategoryByName(name)).thenReturn(category);
-
+        Mockito.when(categoryRepository.findCategoryByAlias(alias)).thenReturn(null);
         String result = categoryService.isCategoryUnique(id, name, alias);
 
         assertThat(result).isEqualTo("Duplicate category name");
+
+    }
+
+    @Test
+    public void testCheckUniqueInNewModeReturnDuplicateAlias(){
+        Integer id = null;
+        String name = "NameABC";
+        String alias = "computers";
+
+        Category category = Category.builder()
+                .name(name)
+                .id(id)
+                .alias(alias)
+                .build();
+
+        Mockito.when(categoryRepository.findCategoryByName(name)).thenReturn(null);
+        Mockito.when(categoryRepository.findCategoryByAlias(alias)).thenReturn(category);
+        String result = categoryService.isCategoryUnique(id, name, alias);
+
+        assertThat(result).isEqualTo("Duplicate category alias");
+
+    }
+
+    @Test
+    public void testCheckUniqueInNewModeReturnOK(){
+        Integer id = null;
+        String name = "NameABC";
+        String alias = "aliasABC";
+
+        Mockito.when(categoryRepository.findCategoryByName(name)).thenReturn(null);
+        Mockito.when(categoryRepository.findCategoryByAlias(alias)).thenReturn(null);
+        String result = categoryService.isCategoryUnique(id, name, alias);
+
+        assertThat(result).isEqualTo("OK");
+
+    }
+
+    @Test
+    public void testCheckUniqueInEditModeReturnDuplicateName(){
+        Integer id = 1;
+        String name = "Computer";
+        String alias = "abc";
+
+        Category category = Category.builder()
+                .name(name)
+                .id(2)
+                .alias(alias)
+                .build();
+
+        Mockito.when(categoryRepository.findCategoryByName(name)).thenReturn(category);
+        Mockito.when(categoryRepository.findCategoryByAlias(alias)).thenReturn(null);
+        String result = categoryService.isCategoryUnique(id, name, alias);
+
+        assertThat(result).isEqualTo("Duplicate category name");
+
+    }
+
+    @Test
+    public void testCheckUniqueInEditModeReturnDuplicateAlias(){
+        Integer id = 1;
+        String name = "NameABC";
+        String alias = "computers";
+
+        Category category = Category.builder()
+                .name(name)
+                .id(2)
+                .alias(alias)
+                .build();
+
+        Mockito.when(categoryRepository.findCategoryByName(name)).thenReturn(null);
+        Mockito.when(categoryRepository.findCategoryByAlias(alias)).thenReturn(category);
+        String result = categoryService.isCategoryUnique(id, name, alias);
+
+        assertThat(result).isEqualTo("Duplicate category alias");
+
+    }
+
+    @Test
+    public void testCheckUniqueInEditModeReturnOK(){
+        Integer id = 1;
+        String name = "NameABC";
+        String alias = "aliasABC";
+
+        Category category = Category.builder()
+                .name(name)
+                .id(id)
+                .alias(alias)
+                .build();
+
+        Mockito.when(categoryRepository.findCategoryByName(name)).thenReturn(null);
+        Mockito.when(categoryRepository.findCategoryByAlias(alias)).thenReturn(category);
+
+        String result = categoryService.isCategoryUnique(id, name, alias);
+
+        assertThat(result).isEqualTo("OK");
 
     }
 
