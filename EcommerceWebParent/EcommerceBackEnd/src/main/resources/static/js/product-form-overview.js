e430690af5438528fe52253abdeb1cc1364c2ddb
VoLeHau <volehau@.gmail>
2022-04-08 14:36:11 +0700

    Khong co het

--- EcommerceWebParent/EcommerceBackEnd/src/main/resources/static/js/product-form-overview.js
+++ EcommerceWebParent/EcommerceBackEnd/src/main/resources/static/js/product-form-overview.js
@@ -13,12 +13,12 @@ $(document).ready(() => {
         id: "richText-editor-sizecolor"
     });
 
-    getCategories();
+    getCategoriesForNewForm();
 
     dropdownBrand.change(() => {
         dropdownCategories.empty();
-
-        getCategoriesForNewForm();
+		
+        getCategories();
     });
 
 
@@ -29,7 +29,7 @@ const getCategoriesForNewForm = () => {
     editMode = false;
 
     if (categoryIdField.length) {
-        editMode(true);
+        editMode=true;
     }
 
     if (!editMode) getCategories();
