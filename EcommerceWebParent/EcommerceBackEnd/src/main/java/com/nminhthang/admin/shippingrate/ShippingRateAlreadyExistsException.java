6b63869fc5a49f2f211a00afdb5e34384ad2ddde
VoLeHau <volehau@.gmail>
2022-04-24 00:43:02 +0700

    Hau update code date 24/04/2022

---
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/nminhthang/admin/shippingrate/ShippingRateAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.nminhthang.admin.shippingrate;
+
+public class ShippingRateAlreadyExistsException extends Exception {
+
+	public ShippingRateAlreadyExistsException(String message) {
+		super(message);
+	}
+
+}
---
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/nminhthang/admin/shippingrate/ShippingRateNotFoundException.java
@@ -0,0 +1,9 @@
+package com.nminhthang.admin.shippingrate;
+
+public class ShippingRateNotFoundException extends Exception {
+
+	public ShippingRateNotFoundException(String message) {
+		super(message);
+	}
+
+}
