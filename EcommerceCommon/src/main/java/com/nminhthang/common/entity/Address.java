c5cdfb66715866fd302698328f5353ab57a405be
nminhthang <rlrisnguyen@gmail.com>
2022-04-25 09:31:47 +0700

    ok

--- EcommerceCommon/src/main/java/com/nminhthang/common/entity/Address.java
+++ EcommerceCommon/src/main/java/com/nminhthang/common/entity/Address.java
@@ -1,22 +1,23 @@
 package com.nminhthang.common.entity;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
 @Entity
 @Table(name = "addresses")
 public class Address {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected Integer id;
+
 	@Column(name = "first_name", nullable = false, length = 45)
 	protected String firstName;
 	
